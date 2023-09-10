2562e576101a7a49914fc3fc30310eb2c66b4543
Amy Waliszewska <69154712+hakierka@users.noreply.github.com>
2023-09-10 22:57:50 +0200

    Update other-files.js

--- code/other-files.js
+++ code/other-files.js
@@ -1,42 +1,40 @@
-// Import external libraries and modules if needed.
-const crypto = require('crypto');
 
 // Helper function for calculating Merkle Tree hash.
 function calculateMerkleHash(left, right) {
-  return crypto.SHA256(left + right).toString();
+ return crypto.SHA256(left + right).toString();
 }
 
 // Custom data structure to represent Merkle Proof.
 class MerkleProof {
-  constructor() {
-    this.proof = [];
-    this.root = null;
-  }
+ constructor() {
+   this.proof = [];
+   this.root = null;
+ }
 
-  addProofNode(node) {
-    this.proof.push(node);
-  }
+ addProofNode(node) {
+   this.proof.push(node);
+ }
 
-  setRoot(root) {
-    this.root = root;
-  }
+ setRoot(root) {
+   this.root = root;
+ }
 }
 
 // Configuration options for the Merkle Proof generation.
 const merkleConfig = {
-  hashAlgorithm: 'sha256',
-  // Add more configuration options here if needed.
+ hashAlgorithm: 'sha256',
+ // Add more configuration options here if needed.
 }
 
 // Error handling code.
 function handleMerkleError(error) {
-  console.error('Merkle Error:', error);
+ console.error('Merkle Error:', error);
 }
 
 // Export the necessary elements for use in merkle.js.
 module.exports = {
-  calculateMerkleHash,
-  MerkleProof,
-  merkleConfig,
-  handleMerkleError,
+ calculateMerkleHash,
+ MerkleProof,
+ merkleConfig,
+ handleMerkleError,
 };
